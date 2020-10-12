a826b012d66d3bd60feb2d7028b210fc894377d6
Darius <1357280829@qq.com>
2020-10-12 11:32:43 +0800

    修补

--- config/shopconfig.php
+++ config/shopconfig.php
@@ -1,1 +1,14 @@
-/usr/local/var/www/ashop-admin/config/shopconfig.php
+<?php
+
+return [
+    'keys' => [
+        'shop_cover_url', 'shop_name', 'shop_desc', 'business_hours',
+    ],
+
+    'default' => [
+        'shop_cover_url' => '',
+        'shop_name'      => '店铺名',
+        'shop_desc'      => '店铺简介',
+        'business_hours' => '营业时间',
+    ],
+];
